5198489cd2e989728339ae315af3d726ef471ddd
Naman Seth <naman.seth_cs19@gla.ac.in>
2020-07-18 19:21:52 +0530

    Primitive Data Types....

---
+++ src/implementation/PrimitiveDataTypes.java
@@ -0,0 +1,15 @@
+package implementation;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        System.out.println("Range of all Primitive Data Types: \n");
+        System.out.println("* Range of Byte: " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        System.out.println("* ASCII Range of Character: " + (int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+        System.out.println("* Range of Short: " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        System.out.println("* Range of Integer: " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.println("* Range of Long: " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        System.out.println("* Range of Float: " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println("* Range of Double: " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+        System.out.println("* Boolean type: " + Boolean.TRUE + " and " + Boolean.FALSE);
+    }
+}
